477294cbc0ebd8db51844dbe13242057f2e4bc7f
Rajasegar Chandran <rajasegar.c@gmail.com>
2019-04-11 12:13:16 +0530

    [TESTS] New test fixtures for callback leaks
    1. Added new test fixtures to check proper handlers are passed
    to remove event listeners

---
+++ transforms/callback-leaks/__testfixtures__/proper-handlers.input.js
@@ -0,0 +1,8 @@
+export default Ember.Component.extend({
+  didInsertElement() {
+    if (this.get('onScroll')) {
+      this._onScrollHandler = (...args) => this.get('onScroll')(...args);
+      window.addEventListener('scroll', this._onScrollHandler);
+    }
+  }
+});
---
+++ transforms/callback-leaks/__testfixtures__/proper-handlers.output.js
@@ -0,0 +1,13 @@
+export default Ember.Component.extend({
+  didInsertElement() {
+    if (this.get('onScroll')) {
+      this._onScrollHandler = (...args) => this.get('onScroll')(...args);
+      window.addEventListener('scroll', this._onScrollHandler);
+    }
+  },
+
+  willDestroyElement: function() {
+    window.removeEventListener("scroll", this._onScrollHandler);
+    this._super(...arguments);
+  }
+});
